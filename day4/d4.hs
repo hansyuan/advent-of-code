7b67e9057b1f4324ebedbf4ae33a82d0fed30346
Hans Yuan <hansy@surveymonkey.com>
2019-01-09 17:06:55 -0800

    starting the attempt to turn the string into actionable types

--- day4/d4.hs
+++ day4/d4.hs
@@ -3,14 +3,23 @@ import Debug.Trace
 f1 = "input"
 f2 = "small_input"
 
+main :: IO ()
 main = do
   contents <- readFile f2
-  print $ lines $ map replace_char contents
+  print $ lines contents
 
 -- | TODO [1518-05-30 00:27] wakes up
+-- Read characters until the closing square bracker ']', and then 
+-- read the rest of the message: 
 
-replace_char :: Char -> Char
-replace_char c
-  | c == '-' = ' '
-  | c == ':' = ' '
-  | otherwise = c
+-- Read [1518-05-30 00:27] as a 5-tuple of ints. Can probably use fold here. 
+datetime :: [Char] -> (Int, Int, Int, Int, Int)
+datetime str = 
+
+-- | Read characters until hit a delimiter, return an int and drop the delimiter
+dtHelper :: [Char] -> (Int, [Char])
+dtHelper [] = []
+dtHelper  =
+
+-- TODO may not want this
+event :: String -> (Int, String)
